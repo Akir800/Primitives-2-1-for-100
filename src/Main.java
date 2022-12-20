65bcc51d4770264505eb5482e376d7812cd31fac
Alex Kir <alexkir800@gmail.com>
2022-12-20 10:52:50 +0200

    add src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int score = 100;//счет клиента (начальные данные).
+        int account = 100;//счет клиента (начальные данные).
 
         int bonus;
-        int deposit = 10000;
+        int deposit = 1500;
 
-        if (deposit >= 10000) {
+        if (deposit >= 1500) {
             bonus = deposit / 100;//формула подсчета бонусных рублей за каждые доп внесенные 100р на депозит.
         } else {
             bonus = 0;
         }
-        int balance = score + deposit + bonus;
+        int initialBalance = account + deposit + bonus;
         System.out.println("Ваш бонус:" + bonus);
-        System.out.println("Ваш Баланс:" + balance);
+        System.out.println("Ваш Баланс:" + initialBalance);
     }
 }
